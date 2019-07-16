56a6dc0babb6c7cf23d5d895befef2c76870ec6f
Mico <qmichael1234@gmail.com>
2019-07-17 01:02:47 +0800

    third commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,13 +7,27 @@
       <img src="https://instagram.fmnl4-3.fna.fbcdn.net/vp/cc39b10d4dc61bde70f221f037d64d03/5DB09DCA/t51.2885-19/s150x150/64436901_314120959533120_4362957732102275072_n.jpg?_nc_ht=instagram.fmnl4-3.fna.fbcdn.net" class="rounded-circle" alt="">
     </div>
     <div class="col-9">
-    <div><h1>quinteromico</h1></div>
-    <div class="d-flex">
-      <div class="pr-3"><strong>42</strong> posts</div>
-      <div class="pr-3"><strong>784</strong> followers</div>
-      <div class="pr-3"><strong>447</strong> following</div>
+      <div><h1>quinteromico</h1></div>
+      <div class="d-flex">
+        <div class="pr-3"><strong>42</strong> posts</div>
+        <div class="pr-3"><strong>784</strong> followers</div>
+        <div class="pr-3"><strong>447</strong> following</div>
+      </div>
+      <div class="pt-4 font-weight-bold">John Michael A. Quintero</div>
+      <div>Untold images.</div>
+      <div><a href="https://github.com/micoaspiras28" target="_blank">https://github.com/micoaspiras28</a></div>
     </div>
-    </div>
+  </div>
+  <hr>
+  <div class="row">
+    <div class="col-4"><img src="/img/img1.jpg" class="responsive-image w-100"></div>
+    <div class="col-4"><img src="/img/img4.jpg" class="responsive-image w-100"></div>
+    <div class="col-4"><img src="/img/img5.jpg" class="responsive-image w-100"></div>
+  </div>
+  <div class="row pt-4">
+    <div class="col-4"><img src="/img/img2.jpg" class="responsive-image w-100"></div>
+    <div class="col-4"><img src="/img/img3.jpg" class="responsive-image w-100"></div>
+    <div class="col-4"><img src="/img/img6.jpg" class="responsive-image w-100"></div>
   </div>
 </div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>mico's untold images</title>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
