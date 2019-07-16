9d71693a64bd952b6c106e36e2cd7158457dbf6a
Mico <qmichael1234@gmail.com>
2019-07-17 00:32:38 +0800

    2nd commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,12 +2,17 @@
 
 @section('content')
 <div class="container">
-  <div class="row">
-    <div class="col-4 p-5" style="background:blue;">
+  <div class="row p-5">
+    <div class="col-3 pr-2" >
       <img src="https://instagram.fmnl4-3.fna.fbcdn.net/vp/cc39b10d4dc61bde70f221f037d64d03/5DB09DCA/t51.2885-19/s150x150/64436901_314120959533120_4362957732102275072_n.jpg?_nc_ht=instagram.fmnl4-3.fna.fbcdn.net" class="rounded-circle" alt="">
     </div>
-    <div class="col-8" style="background:red;">
-asdas
+    <div class="col-9">
+    <div><h1>quinteromico</h1></div>
+    <div class="d-flex">
+      <div class="pr-3"><strong>42</strong> posts</div>
+      <div class="pr-3"><strong>784</strong> followers</div>
+      <div class="pr-3"><strong>447</strong> following</div>
+    </div>
     </div>
   </div>
 </div>
