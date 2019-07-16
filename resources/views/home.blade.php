331a3dfe675fcc77e6204d308e132839dd69ed01
Mico <qmichael1234@gmail.com>
2019-07-16 23:46:58 +0800

    1st Commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+  <div class="row">
+    <div class="col-4 p-5" style="background:blue;">
+      <img src="https://instagram.fmnl4-3.fna.fbcdn.net/vp/cc39b10d4dc61bde70f221f037d64d03/5DB09DCA/t51.2885-19/s150x150/64436901_314120959533120_4362957732102275072_n.jpg?_nc_ht=instagram.fmnl4-3.fna.fbcdn.net" class="rounded-circle" alt="">
+    </div>
+    <div class="col-8" style="background:red;">
+asdas
+    </div>
+  </div>
+</div>
+@endsection
